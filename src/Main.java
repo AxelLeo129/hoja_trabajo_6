a5580892d250362656f4d75e170ba12168ce0063
AxelLeo129 <axelleo129dev@gmail.com>
2021-03-18 00:25:50 -0600

    Main class

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+package src;
+
+/**
+ * @author Axel Lopez
+ * @author Luis Gonzalez
+ * GIT: https://github.com/LPELCRACK896/hojaDeTrabajo4ALgoritmos.git
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");   
+    }
+    
+}
